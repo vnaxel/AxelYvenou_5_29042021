c3cd5c38ce2e79b491e5c17d30118c072692a358
Axel Yvenou <axel.yvenou@gmail.com>
2021-05-13 23:53:15 +0200

    order function created // need RegEx tests in the getContact function and display the ThankYou page with order totalPrice and orderId

--- front/cart.js
+++ front/cart.js
@@ -4,7 +4,7 @@ displayCart()
 
 function displayCart () {
     if (window.localStorage.length === 0) {
-        cart.innerHTML = "Panier vide"
+        cart.innerHTML = "cart is empty"
     }else{
         Object.keys(localStorage).map((key) => {
             let item = localStorage.getItem(key)
@@ -35,7 +35,7 @@ function displayCart () {
                     displayItem.remove()
                     displayOrderTotalPrice()
                     if (window.localStorage.length === 0) {
-                        cart.innerHTML = "Panier vide"
+                        cart.innerHTML = "cart is empty"
                     }
                     return
                 }
@@ -50,9 +50,8 @@ function displayCart () {
 
             })
 
-            deleteBtn.addEventListener('click', (e) => {
+            deleteBtn.addEventListener('click', () => {
                 localStorage.removeItem(key)
-                e.stopPropagation()
                 displayItem.remove()
                 cart.innerHTML = ""
                 displayCart()
@@ -87,4 +86,75 @@ function displayCart () {
             }
         }
     }
+}
+
+
+
+//  --- FORM & ORDER ---
+
+const orderBtn = document.getElementById('order')
+orderBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    order()
+})
+
+function order () {
+    
+    postOrder()
+
+// recupere et cree l'objet contact depuis le formulaire
+    function getContact () {
+
+        const firstName = document.getElementById('firstName').value
+        const lastName = document.getElementById('lastName').value
+        const adress = document.getElementById('adress').value
+        const city = document.getElementById('city').value
+        const email = document.getElementById('email').value
+
+        const contact = {
+            firstName : firstName,
+            lastName : lastName,
+            address : adress,
+            city : city,
+            email : email
+        }
+        return contact
+    }
+    
+// recupere les Id des produit dans le local storage et crée l'objet produits
+    function getProductIds () {
+        const products = []
+
+        Object.keys(localStorage).map((key) => {
+            const product = JSON.parse(localStorage.getItem(key))
+            const productId = product.id
+            for (let i = 0; i < Number(product.amount); i++) {
+            products.push(productId)
+            }
+        })
+        return products
+    }
+
+// post l'objet order
+    async function postOrder () {
+        const contact = getContact()
+        const products = getProductIds()
+        const order = {
+            contact: contact,
+            products: products
+        }
+        console.log(order)
+        return fetch("http://localhost:3000/api/furniture/order", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res)
+        })
+    }
+
 }
